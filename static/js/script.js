bdca30e084a15ab3e179c829ef26f4b425d04b6a
Aramantos <john.doyle.mail@icloud.com>
2021-05-09 16:17:54 +0000

    implemented log-reg buttons and register panel with styles and scripts

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,16 @@
  $(document).ready(function(){
+
+    // Materialize Scripts
     $('.sidenav').sidenav({edge: "right"});
-  });
+
+    // Original Scripts
+    $('#login-btn').click(function() {
+        $('#login-panel').css('visibility', 'visible');
+        $('#login-panel').css('display', 'block');
+        $('#reg-panel').css('display', 'none');
+    })
+    $('#reg-btn').click(function() {
+        $('#login-panel').css('display', 'none');
+        $('#reg-panel').css('display', 'block');
+    })
+  }); 
