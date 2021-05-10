3ee0fa575c18c8d945860ff84b05a236d4e1d025
Aramantos <john.doyle.mail@icloud.com>
2021-05-10 10:27:31 +0000

    added logout functionality

--- app.py
+++ app.py
@@ -82,6 +82,15 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash(" - You have been logged out - ")
+    # session.clear()
+    session.pop("user")
+    return render_template("login-register.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
