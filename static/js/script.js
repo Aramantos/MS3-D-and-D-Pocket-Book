6c547dc6da9474e3f757334db11df536fa226afc
Aramantos <john.doyle.mail@icloud.com>
2021-05-11 13:28:03 +0000

    added scripts so cancel buttons remove panel

--- static/js/script.js
+++ static/js/script.js
@@ -13,13 +13,23 @@
         $('#reg-panel').css('display', 'none');
     })
     $('#reg-btn').click(function() {
-        $('#login-panel').css('display', 'none');
+        $('#reg-panel').css('visibility', 'visible');
         $('#reg-panel').css('display', 'block');
+        $('#login-panel').css('display', 'none');
+    })
+    $('#log-cancel').click(function() {
+        $('#login-panel').css('visibility', 'hidden');
+    })
+    $('#reg-cancel').click(function() {
+        $('#reg-panel').css('visibility', 'hidden');
     })
 
     //profile.html
     $('#game-add-btn').click(function() {
         $('#game-add-panel').css('display', 'block');
+    })
+    $('#game-add-cancel').click(function() {
+        $('#game-add-panel').css('display', 'none');
     })
 
     //login-register.html
