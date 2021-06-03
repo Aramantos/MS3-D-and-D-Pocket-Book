d518d94ac7f09625ad2a30da7ffa1f4767dbd018
Aramantos <john.doyle.mail@icloud.com>
2021-06-03 11:10:06 +0000

    update error handler 405

--- app.py
+++ app.py
@@ -558,10 +558,10 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
-# 405 Error Server Error
+# 405 Error Method
 @app.errorhandler(405)
 def method_not_allowed(error):
-    return redirect(url_for("405.html"), 405)
+    return render_template('405.html'), 405
 
 
 # 500 Error Server Error
